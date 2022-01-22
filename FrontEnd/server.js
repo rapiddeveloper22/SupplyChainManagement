bc9b40c1a8fe5a6002e53a14d45469ffdae91e0a
rapiddeveloper22 <mmaswin22@gmail.com>
2022-01-22 19:50:57 +0530

    a

--- FrontEnd/server.js
+++ FrontEnd/server.js
@@ -1,23 +1,11 @@
-var express=require('express');
+var express = require('express');
 
-var app=express();
+var app = express();
 
-app.get('/', function (req, res) 
-{
-    res.sendFile( __dirname + "/" + "index.html" );
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/" + "homepage.html");
 })
 
-app.get("/register",function(req,res){
-    details = {
-        name : req.query.name,
-        email : req.query.email,
-        password : req.query.password,
-        type : req.query.type
-    }
-
-});
-        
-   
 var server = app.listen(8080, function () {
     var host = server.address().address;
     var port = server.address().port;
